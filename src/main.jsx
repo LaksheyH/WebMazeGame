bba0ab2106f7f0cdc554fe103293312931f4f3bc
Lakshey Hooda <98902414+LaksheyH@users.noreply.github.com>
2022-02-19 07:41:00 +0000

    Collisions Visualization

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import Test from "./TestingMaze.jsx"
-import FullMaze from "./FullMaze.jsx"
 import CanvasBasedMaze from './CanvasBasedMaze'
 
 ReactDOM.render(
